bdd78ed9201a67250bf63da7d0ec69843680cede
Victor Mbachu <victor.c.mbachu@gmail.com>
2023-01-05 10:27:21 +0000

    update

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -14,6 +14,7 @@ class UserResource extends JsonResource
      */
     public function toArray($request){
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'mobile' => $this->mobile,
